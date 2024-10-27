7c3d989b73213cb8afeb462d2e2af803c239f72f
Muaadh Adeleye <muaadhadeleye@gmail.com>
2024-10-27 18:28:54 +0100

    fifth commit

--- library_system/settings.py
+++ library_system/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 import os
-import dj_database_url
+
 
 from pathlib import Path
 
