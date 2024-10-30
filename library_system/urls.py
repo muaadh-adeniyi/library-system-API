94eeebcc6acd562924c13f6b1adc33d949d4e17b
Muaadh Adeleye <muaadhadeleye@gmail.com>
2024-10-30 02:42:46 +0100

    seventh commit

--- library_system/urls.py
+++ library_system/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
-    path('', home, name='home'),
+    path('home/', home, name='home'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
