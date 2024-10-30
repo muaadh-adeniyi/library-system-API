da02b65c1270e84639e58bfcf285d8ecc9c18c32
Muaadh Adeleye <muaadhadeleye@gmail.com>
2024-10-30 01:11:47 +0100

    seventh commit

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,10 @@
-from  django.urls import path
+from django.urls import path
 from . import views
 
-
 urlpatterns = [
-    path('v1/books/', views.list_book, name='list-book'),
-    path('v1/book/<uuid:pk>/', views.get_book, name='get-book'),
-    path('v1/book/', views.create_book, name='create-book'),
-    path('v1/books/<uuid:pk>/', views.update_book, name='update-book'),
-    path('v1/book/<uuid:pk>', views.delete_book, name='delete-book'),
+    path('v1/books/', views.list_book, name='list-book'),  # List all books
+    path('v1/book/<uuid:pk>/', views.get_book, name='get-book'),  # Retrieve a book by ID
+    path('v1/book/', views.create_book, name='create-book'),  # Create a new book
+    path('v1/book/<uuid:pk>/', views.update_book, name='update-book'),  # Update a book
+    path('v1/book/<uuid:pk>/', views.delete_book, name='delete-book'),  # Delete a book
 ]
